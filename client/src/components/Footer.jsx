52daf89dda69550b1e783c6122fe1637e2446d48
Carlos Juan Carabajal <carjuancara@gmail.com>
2024-01-19 07:35:04 -0300

    fix: :adhesive_bandage: agregue mb-auto al contenedor principal del FOOTER

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer () {
   return (
 
-    <footer className='text-gray-600 body-font'>
+    <footer className='text-gray-600 body-font mb-auto'>
       <div className='container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col'>
         <a className='flex title-font font-medium items-center md:justify-start justify-center text-gray-900'>
           <svg xmlns='http://www.w3.org/2000/svg' fill='none' stroke='currentColor' className='w-10 h-10 text-white p-2 bg-indigo-500 rounded-full' viewBox='0 0 24 24'>
