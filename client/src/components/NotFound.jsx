0b37f88c1fdadfe5e643bd90a22e087ecae7ca88
Carlos Juan Carabajal <carjuancara@gmail.com>
2024-01-19 07:11:07 -0300

    feat: :sparkles: agrego componete NOT FOUND
    Muestra UN MENSAJE cuando la busqueda de algun pais POR NOMBRE PARCIAL no tiene resultados

---
+++ client/src/components/NotFound.jsx
@@ -0,0 +1,7 @@
+export const NotFound = () => {
+  return (
+    <div className='container flex justify-center w-screen'>
+      <h3 className='my-52 text-5xl flex justify-center items-center w-screen'>No se encontraron resultados</h3>
+    </div>
+  )
+}
