f87290da34edb74d7cf90699445778bcba67ee66
Carlos Juan Carabajal <carjuancara@gmail.com>
2024-01-29 18:49:39 -0300

    feat: :sparkles: agregue boton a la pagina /home

--- client/src/components/NewActivity.jsx
+++ client/src/components/NewActivity.jsx
@@ -4,7 +4,6 @@ import axios from 'axios'
 import { useSelector } from 'react-redux'
 import { useState } from 'react'
 import Alerts from './Alerts'
-import { Link } from 'react-router-dom'
 const SignupSchema = Yup.object().shape({
   name: Yup.string()
     .min(6, 'El nombre es demasiado corto!')
@@ -190,28 +189,6 @@ function NewActivity () {
                 </button>
               ))}
 
-          </div>
-          <div className='flex justify-end items-center w-full h-10 pr-60'>
-            <Link
-              to='/home'
-              className='flex h-10 px-4 rounded-lg text-neutral-900 gap-4 items-center bg-secundario '
-            >
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                strokeWidth={1.5}
-                stroke='currentColor'
-                className='w-6 h-6'
-              >
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  d='M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3'
-                />
-              </svg>
-              Volver al inicio
-            </Link>
           </div>
         </div>
       </div>
