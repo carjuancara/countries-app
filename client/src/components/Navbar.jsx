a97479842fe567d12953edabd140dfa75d6b0026
Carlos Juan Carabajal <carjuancara@gmail.com>
2024-01-29 18:52:11 -0300

    feat: :lipstick: agregue boton condicional a /home

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -86,7 +86,30 @@ const Navbar = () => {
                 <path strokeLinecap='round' strokeLinejoin='round' d='m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z' />
               </svg>
             </button>}
-
+          {((location.slice(0, -4) === '/countriesdetail') || (location === '/newactivity')) && (
+            <div className='flex justify-end items-center w-full h-10 pr-60'>
+              <Link
+                to='/home'
+                className='flex h-10 px-4 rounded-lg text-neutral-900 gap-4 items-center bg-secundario '
+              >
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  strokeWidth={1.5}
+                  stroke='currentColor'
+                  className='w-6 h-6'
+                >
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    d='M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3'
+                  />
+                </svg>
+                Volver al inicio
+              </Link>
+            </div>
+          )}
         </div>
 
       </div>
