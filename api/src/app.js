41bc33bdffd44825c0112073b405a20a5f9f55b3
Carlos Juan Carabajal <carjuancara@gmail.com>
2024-02-07 13:05:11 -0300

    feat: :heavy_plus_sign: agregue cors al servidor

--- api/src/app.js
+++ api/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const routes = require('./routes/index.js')
+const cors = require('cors')
 
 require('./db.js')
 
@@ -9,10 +10,11 @@ const server = express()
 server.name = 'API'
 server.use(express.json({ limit: '1mb' }))
 server.use(morgan('dev'))
+server.use(cors())
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*') // update to match the domain you will make the request from
+  // res.header('Access-Control-Allow-Origin', '*') // update to match the domain you will make the request from
+  // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   res.header('Access-Control-Allow-Credentials', 'true')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
   next()
 })
