7f0664ff99178b5b5e24e7534b5889581351f02e
Carlos Juan Carabajal <carjuancara@gmail.com>
2024-02-07 09:35:18 -0300

    fix: :adhesive_bandage: adicione /countries a la BASE_URL

--- client/src/redux/countriesAction.js
+++ client/src/redux/countriesAction.js
@@ -6,7 +6,7 @@ export const BASE_URL = import.meta.env.VITE_BASE_URL
 
 // trae todos los paises al inicio
 export const getAllCountries = () => (dispatch) => {
-  axios(`${BASE_URL}`)
+  axios(`${BASE_URL}/countries`)
     .then(res => {
       dispatch(updateAllCountries(res.data))
     })
